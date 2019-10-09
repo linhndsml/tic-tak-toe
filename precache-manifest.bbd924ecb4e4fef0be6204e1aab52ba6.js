be85701ddb2f16339ed3b874f1fc828ba5f8f601
SML <Admin@SMLs-MacBook-Pro.local>
2019-10-09 21:27:38 +0700

    Updates

---
+++ precache-manifest.bbd924ecb4e4fef0be6204e1aab52ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f5aa3b2db388ee199d314863439c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140b5d9864a53d1d399a",
+    "url": "/static/css/main.49c5eed2.chunk.css"
+  },
+  {
+    "revision": "35a38fd32645e363c2bd",
+    "url": "/static/js/2.af230678.chunk.js"
+  },
+  {
+    "revision": "140b5d9864a53d1d399a",
+    "url": "/static/js/main.f6d6d994.chunk.js"
+  },
+  {
+    "revision": "a1b154eec4da01f08524",
+    "url": "/static/js/runtime-main.30c7ec2c.js"
+  }
+]);
